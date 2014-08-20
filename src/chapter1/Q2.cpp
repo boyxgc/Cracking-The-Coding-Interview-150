cd1fab3b04b46889e0815175437712fee02c0bc2
boyxgc <boyxgc@gmail.com>
2014-08-20 11:39:19 -0700

    add cpp solution for Q2

---
+++ src/chapter1/Q2.cpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *       Filename:  Q2.cpp
+ *        Created:  08/20/2014 11:22:20
+ *       Compiler:  gcc
+ *
+ *         Author:  Devin Guocheng Xie (gc), guochenx@usc.edu
+ * =====================================================================================
+ */
+#include <iostream>
+using namespace std;
+
+
+char* reverse(char * str) {
+    if(!str) return NULL;
+
+    char * head = str;
+    char * end = str;
+    while(*end) {
+        ++end;
+    }
+
+    --end;
+
+    while(head < end) {
+        char tmp = *head;
+        *head++ = *end;
+        *end-- = tmp;
+    }
+
+    return str;
+}
+
+int main() {
+    char * str = new char[10];
+    strcpy(str, "hello\0world");
+
+    printf("#%s#\n", reverse(str));
+    return 0;
+}
