b3adf9258f87917a5f2075f25adb0ff70b0455bb
boyxgc <boyxgc@gmail.com>
2014-08-20 11:21:11 -0700

    add new o(n) solution

--- src/chapter1/Q3.java
+++ src/chapter1/Q3.java
@@ -4,7 +4,14 @@ import java.util.Arrays;
 
 public class Q3 {
 
+	/*
+	 * time complexity O(n*log(n))
+	 */
 	public static boolean isPermutationOfEachOther(String str1, String str2) {
+		if (str1 == null || str2 == null || str1.length() != str2.length()) {
+			return false;
+		}
+
 		return sort(str1).equals(sort(str2));
 	}
 
@@ -18,8 +25,36 @@ public class Q3 {
 		return sortedStr;
 	}
 
+	/*
+	 * Assuming the two strings only contains ASCII characters time complexity
+	 * O(n)
+	 */
+	public static boolean isPermutationOfEachOther2(String str1, String str2) {
+		if (str1 == null || str2 == null || str1.length() != str2.length()) {
+			return false;
+		}
+
+		int charCount[] = new int[256];
+
+		for (char c : str1.toCharArray()) {
+			charCount[(int) c]++;
+		}
+
+		for (char c : str2.toCharArray()) {
+			int newCount = --charCount[(int) c];
+			if (newCount < 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(isPermutationOfEachOther("hello", "llhoe"));
 		System.out.println(isPermutationOfEachOther("hello", "llhoed"));
+
+		System.out.println(isPermutationOfEachOther2("hello", "llhoe"));
+		System.out.println(isPermutationOfEachOther2("hello", "llhee"));
 	}
 }
