9b5a7ac2c018343c32a81a27fcae399bea3c9661
boyxgc <boyxgc@gmail.com>
2014-08-20 10:52:09 -0700

    answer v1.0

---
+++ src/chapter1/Q2.java
@@ -0,0 +1,28 @@
+package chapter1;
+
+public class Q2 {
+
+	/*
+	 * Using StringBuffer, time complexity O(n)
+	 */
+	public static String reverse(String inputStr) {
+		int nullIndex = inputStr.indexOf("\0");
+		nullIndex = (nullIndex == -1) ? inputStr.length() : nullIndex;
+		
+		/*get null-terminated string*/
+		String stringBeforeNull = inputStr.substring(0, nullIndex);
+		
+		StringBuffer reverseString = new StringBuffer();
+		
+		for (int i = stringBeforeNull.length() - 1; i >= 0; i--) {
+			reverseString.append(inputStr.charAt(i));
+		}
+		
+		return reverseString.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("#" + reverse("hello world\0fsfas") + "#");
+		System.out.println("#" + reverse("") + "#");
+	}
+}
