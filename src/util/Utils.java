d634c44f1c1f5dbe1c93fa596bc91c6f64bb59d5
boyxgc <boyxgc@gmail.com>
2014-08-20 17:45:38 -0700

    add print linkedlist method

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -1,12 +1,22 @@
 package util;
 
+import dataStructure.LinkedListNode;
+
 public class Utils {
-	public static void printIntMatrix(int [][] matrix, int m, int n) {
+	public static void printIntMatrix(int[][] matrix, int m, int n) {
 		for (int i = 0; i < m; ++i) {
 			for (int j = 0; j < n; ++j) {
 				System.out.print(matrix[i][j] + "  ");
 			}
 			System.out.println();
 		}
+	}
+
+	public static void printLinkedList(LinkedListNode<?> head) {
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+		System.out.println();
 	}
 }
