710d0913b878921eb58fa50a0c2cf070a26939bc
boyxgc <boyxgc@gmail.com>
2014-08-22 18:26:22 -0700

    add solution to Q4.3

---
+++ src/chapter4/Q3.java
@@ -0,0 +1,39 @@
+package chapter4;
+
+import util.Utils;
+import dataStructure.BiTreeNode;
+
+public class Q3 {
+
+	public static <T> BiTreeNode<T> createMinHeightBST(T[] data) {
+		return createMinHeightBST(data, 0, data.length - 1);
+	}
+
+	/*
+	 * select the middle one as the root of the tree, do this recursively
+	 */
+	private static <T> BiTreeNode<T> createMinHeightBST(T[] data,
+			int fromIndex, int toIndex) {
+
+		if (fromIndex > toIndex) {
+			return null;
+		}
+
+		int middleIndex = (fromIndex + toIndex) / 2;
+
+		BiTreeNode<T> root = new BiTreeNode<T>(data[middleIndex]);
+
+		root.left = createMinHeightBST(data, fromIndex, middleIndex - 1);
+		root.right = createMinHeightBST(data, middleIndex + 1, toIndex);
+
+		return root;
+	}
+
+	public static void main(String[] args) {
+
+		Integer data[] = { 1, 2, 3, 4, 5, 6, 7 };
+		BiTreeNode<Integer> root = createMinHeightBST(data);
+		
+		Utils.printBiTreePreorder(root);
+	}
+}
