556df529737d64995ce2d722b412380986c0a1bc
boyxgc <boyxgc@gmail.com>
2014-08-26 21:04:18 -0700

    add approaches to handle OOD

--- src/chapter8/package-info.java
+++ src/chapter8/package-info.java
@@ -1,5 +1,16 @@
 /**
  * Object-Oriented Design
+ * 
+ * Approaches:
+ * 
+ * 1. Handle Ambiguity
+ * 
+ * 2. Define Core Objects
+ * 
+ * 3. Analyze Relationships
+ * 
+ * 4. Investigate Actions
+ * 
  */
 /**
  * @author boyxgc
