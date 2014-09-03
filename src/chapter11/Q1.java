f06b7ab9a125738f7159d62775692de1e046041c
boyxgc <boyxgc@gmail.com>
2014-09-02 20:12:11 -0700

    [optimize][Q11.1]merge two array

--- src/chapter11/Q1.java
+++ src/chapter11/Q1.java
@@ -13,24 +13,23 @@ public class Q1 {
 		int pos = na + nb - 1;
 		int pos_a = na - 1;
 		int pos_b = nb - 1;
-		while (pos >= 0) {
-			if (pos_a >= 0
-					&& ((pos_b >= 0 && A[pos_a] < B[pos_b]) || pos_b < 0)) {
-				A[pos--] = A[pos_a--];
-			} else if (pos_b >= 0
-					&& ((pos_a >= 0 && B[pos_b] <= A[pos_a]) || pos_a < 0)) {
-				A[pos--] = B[pos_b--];
+		while (pos_b >= 0) {
+			if (pos_a >= 0 && A[pos_a] <= B[pos_b]) {
+				A[pos] = A[pos_a--];
+			} else {
+				A[pos] = B[pos_b--];
 			}
+			pos--;
 		}
 
 		return na + nb;
 	}
 
 	public static void main(String[] args) {
-		int A[] = { 10, 8, 5, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		int B[] = { 11, 10, 9 };
+		int A[] = { 10, 8, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		int B[] = { 11, 10, 9, 3, 1 };
 
-		int newlen = merge(A, 5, B, 3);
+		int newlen = merge(A, 3, B, 5);
 
 		for (int i = 0; i < newlen; ++i) {
 			System.out.print(A[i] + " ");
