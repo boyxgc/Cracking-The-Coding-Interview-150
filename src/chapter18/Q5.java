0a0e8e8603cd012338fa27702763160cea588cba
boyxgc <boyxgc@gmail.com>
2014-09-07 22:55:19 -0700

    [Q18.5]find min distance between two words

---
+++ src/chapter18/Q5.java
@@ -0,0 +1,18 @@
+package chapter18;
+
+/**
+ * You have a large text file containing words. Given any two words, find the
+ * shortest distance (in terms of number of words) between them in the file. If
+ * the operation will be repeated many times for the same file (but different
+ * pairs of words), can you optimize your solution
+ * 
+ * @author boyxgc
+ * 
+ */
+public class Q5 {
+
+	/*
+	 * create a hash table to store the position of each word, traverse through
+	 * all the positions to find the minimum distance
+	 */
+}
